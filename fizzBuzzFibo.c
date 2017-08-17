3865565415d94999983aa3b5636b19a44c62dddd
RRShanbhag <ranjit.shanbhag@gmail.com>
2017-08-17 12:26:53 -0600

    FizzBuzz Fibonacci Series Complete

--- fizzBuzzFibo.c
+++ fizzBuzzFibo.c
@@ -2,26 +2,48 @@
 #include <stdlib.h>
 
 void fibonacci(int N);
-void checkFizzBuzz(int Number);
+void checkFizzBuzz(long Number);
+int isprime(long Number);
 
-
-void checkFizzBuzz(int Number)
+int isprime(long Number)
 {
-	if(Number % 3 == 0)
+	int i, flag=1;
+	if(Number < 2)
+		flag = 0;
+    for(i=2; i<=Number/2; ++i)
+    {
+        if(Number%i==0)
+        {
+            flag=0;
+            break;
+        }
+    }
+    return flag;
+}
+
+void checkFizzBuzz(long Number)
+{
+	if (Number % 15 == 0)
+		printf(", FizzBuzz");
+	else if(Number % 3 == 0)
 		printf(", Buzz");
+	else if (Number % 5 == 0)
+		printf(", Fizz");
+	else if (isprime(Number))
+		printf(", BuzzFizz");
 	else
-		printf(", %d", Number);
+		printf(", %ld", Number);
 	return;
 }
 
 void fibonacci (int N)
 {
-	int a = 0;
-	int b = 1;
-	printf("%d", b);
+	long a = 0;
+	long b = 1;
+	printf("%ld", b);
 	for(int i = 1; i < N; i++)
 	{
-		int c = a + b;
+		long c = a + b;
 		a = b;
 		b = c;
 		checkFizzBuzz(c);
@@ -32,9 +54,13 @@ void fibonacci (int N)
 int main(int argc, char *argv[])
 {
 	int N = 0;
-	printf("Enter the Series Limit\n");
+	printf("Enter the Series Limit: Maximum Limit 50\n");
 	scanf("%d", &N);
-
+	if (N > 50)
+	{
+		printf("Error: Maximum Limit Crossed\n");
+		exit(0);
+	}
 	fibonacci(N);
 	return 0;
 }
